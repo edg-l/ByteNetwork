fff90599ae3b53b4b5f66f301a7dbda2fb17b070
Edgar Luque <clud6565@gmail.com>
2017-08-12 17:32:58 +0200

    cleanup

--- ByteNetwork/Client.cs
+++ ByteNetwork/Client.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Net.Sockets;
+﻿using System.Net.Sockets;
 using System.Net;
-using System.Threading;
 
 namespace ByteNetwork
 {
--- ByteNetwork/Properties/AssemblyInfo.cs
+++ ByteNetwork/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ByteNetwork")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A simple networking library.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ByteNetwork")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 Edgar Luque")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
